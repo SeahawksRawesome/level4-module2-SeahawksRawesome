062b0326d97cc851d430f4ae65da376f3994c4e2
League <league@iMac-3.local>
2019-09-29 17:24:23 -0700

    Yay doing stuff

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,13 +32,30 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if(s1.length() > s2.length()) {
+			return s1;
+		}
+		if(s2.length() > s1.length()) {
+			return s2;
+		}
+		else {
+			return "equal";
+		}
+		//return null;
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if(s.contains("underscores")) {
+			for(int i = 0; i < s.length(); i++) {
+				if(s.substring(i, i+1).equals(" ")) {
+					s.replace(s.charAt(i), '_');
+					
+				}
+			}
+		}
+		return s;
 	}
 
 	
@@ -46,13 +63,59 @@ public class StringMethods {
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		char one = 0;
+		char two = 0;
+		char three = 0;
+		for(int i = s1.length() -1; i > 0; i--) {
+			if(s1.charAt(i) != ' ') {
+				one = s1.charAt(i);
+				break;
+			}
+		}
+		for(int i = s2.length() -1; i > 0; i--) {
+			if(s2.charAt(i) != ' ') {
+				two = s2.charAt(i);
+				break;
+			}
+		}
+		for(int i = s3.length() -1; i > 0; i--) {
+			if(s3.charAt(i) != ' ') {
+				three = s3.charAt(i);
+				break;
+			}
+		}
+		if(Character.getNumericValue(one) > Character.getNumericValue(two)) {
+			if(Character.getNumericValue(one) > Character.getNumericValue(three)) {
+				return s1;
+			}
+		}
+		if(Character.getNumericValue(two) > Character.getNumericValue(one)) {
+			if(Character.getNumericValue(two) > Character.getNumericValue(three)) {
+				return s2;
+			}
+		}
+		if(Character.getNumericValue(three) > Character.getNumericValue(two)) {
+			if(Character.getNumericValue(three) > Character.getNumericValue(one)) {
+				return s3;
+			}
+		}
+		return "";
+		
 	}
 	
 	
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int total = 0;
+		char converter;
+		int numba;
+		for(int i = 0; i < s.length(); i++) {
+			if(Character.isDigit(s.charAt(i))) {
+				numba = Character.getNumericValue(s.charAt(i));
+				total += numba;
+			}
+		}
+		return total;
 	}
 	
 	
